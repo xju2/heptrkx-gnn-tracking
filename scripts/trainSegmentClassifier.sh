5965df6cb6c422a2dd795bf6b1d921f44b8024c1
xju <xju@cern.ch>
2019-01-18 15:03:55 -0800

    up

--- scripts/trainSegmentClassifier.sh
+++ scripts/trainSegmentClassifier.sh
@@ -2,6 +2,10 @@
 #SBATCH -C haswell
 #SBATCH -q debug
 #SBATCH -t 30
+#SBATCH --mail-type=BEGIN,END
+#SBATCH --mail-user=xju@lbl.gov
+#SBATCH -N 4
 
 . scripts/setup.sh
-srun -l python ./train.py -d configs/segclf.yaml
+#srun -n 128 -l python ./train.py -d configs/segclf.yaml
+srun -n 128 -l python ./train.py -d configs/hello_test.yaml
