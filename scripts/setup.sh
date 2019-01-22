a08bd011089aa837ecd157d9e2c0bd27607bc4de
xju <xju@cern.ch>
2019-01-22 12:19:12 -0800

    a working flow for me

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,3 +3,4 @@ export OMP_NUM_THREADS=32
 export KMP_AFFINITY="granularity=fine,compact,1,0"
 export KMP_BLOCKTIME=1
 module load pytorch/v1.0.0-intel
+export PYTHONPATH=$PYTHONPATH:/global/homes/x/xju/track/gnn/code/python_pkg/install/lib/python3.7/site-packages
