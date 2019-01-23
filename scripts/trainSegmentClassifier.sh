cf5d37846e4e984a9c3ef6500e83b04a28bf0431
xju <xju@cern.ch>
2019-01-23 09:43:49 -0800

    up

--- scripts/trainSegmentClassifier.sh
+++ scripts/trainSegmentClassifier.sh
@@ -1,20 +1,12 @@
 #!/bin/bash
-#SBATCH -J distributed
+#SBATCH -J segclf-big
 #SBATCH -C haswell
 #SBATCH -q debug
 #SBATCH -t 30
-#SBATCH --mail-type=BEGIN,END
-#SBATCH --mail-user=xju@lbl.gov
-#SBATCH -N 4
 #SBATCH -o logs/%x-%j.out
 
 
 mkdir -p logs
 . scripts/setup.sh
 
-#srun -n 128 -l python ./train.py -d configs/segclf.yaml
-srun -l python ./train.py -d configs/test_distributed.yaml
-
-
-#srun -l python ./train.py -v -d configs/segclf_big.yaml
-
+srun -l python ./train.py -v -d configs/segclf_big.yaml
