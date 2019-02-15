5f50c3a0e6a0251ef65be731f7a9bef589227a37
xju <xju@cern.ch>
2019-02-15 11:48:07 -0800

    separate utility functions from exe

--- train_nx_graph_with_config.py
+++ train_nx_graph_with_config.py
@@ -5,63 +5,19 @@ import sklearn.metrics
 
 import tensorflow as tf
 
-from graph_nets import modules
 from graph_nets import utils_tf
 from graph_nets import utils_np
 
-import glob
-import re
-import yaml
-
-def create_feed_dict(generator, batch_size, input_ph, target_ph):
-    inputs, targets = generator(batch_size)
-    input_graphs = utils_np.networkxs_to_graphs_tuple(inputs)
-    target_graphs = utils_np.networkxs_to_graphs_tuple(targets)
-    feed_dict = {input_ph: input_graphs, target_ph: target_graphs}
-
-    return feed_dict
-
-
-def create_loss_ops(target_op, output_ops):
-    # only use edges
-    loss_ops = [
-        tf.losses.sigmoid_cross_entropy(target_op.edges, output_op.edges)
-        for output_op in output_ops
-    ]
-    return loss_ops
-
-
-def make_all_runnable_in_session(*args):
-  """Lets an iterable of TF graphs be output from a session as NP graphs."""
-  return [utils_tf.make_runnable_in_session(a) for a in args]
-
-
-def computer_matrics(target, output):
-    tdds = utils_np.graphs_tuple_to_data_dicts(target)
-    odds = utils_np.graphs_tuple_to_data_dicts(output)
-
-    test_target = []
-    test_pred = []
-    for td, od in zip(tdds, odds):
-        test_target.append(td['edges'])
-        test_pred.append(od['edges'])
-
-    test_target = np.concatenate(test_target, axis=0)
-    test_pred   = np.concatenate(test_pred,   axis=0)
-
-    thresh = 0.5
-    y_pred, y_true = (test_pred > thresh), (test_target > thresh)
-    return sklearn.metrics.precision_score(y_true, y_pred), sklearn.metrics.recall_score(y_true, y_pred)
-
-
-def load_config(config_file):
-    with open(config_file) as f:
-        return yaml.load(f)
-
+from nx_graph.utils_train import create_feed_dict
+from nx_graph.utils_train import create_loss_ops
+from nx_graph.utils_train import make_all_runnable_in_session
+from nx_graph.utils_train import compute_matrics
 
 if __name__ == "__main__":
     import sys
     import argparse
+    import glob
+    import re
 
     parser = argparse.ArgumentParser(description='Train nx-graph with configurations')
     add_arg = parser.add_argument
@@ -172,7 +128,7 @@ if __name__ == "__main__":
                 "loss": loss_op_tr,
                 "outputs": output_ops_tr
             }, feed_dict=feed_dict)
-            correct_tr, solved_tr = computer_matrics(
+            correct_tr, solved_tr = compute_matrics(
                 test_values["target"], test_values["outputs"][-1])
             elapsed = time.time() - start_time
             losses_tr.append(train_values["loss"])
