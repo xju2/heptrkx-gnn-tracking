84163ca2603ced511b3352524bb2c3eb8129b5a5
xju <xju@cern.ch>
2019-06-12 21:45:38 -0700

    protect from missing files

--- slurm/submit_make_doublets.sh
+++ slurm/submit_make_doublets.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH -J doublets
 #SBATCH -C haswell
-#SBATCH -q debug
-#SBATCH -t 00:30:00
+#SBATCH -q regular
+#SBATCH -t 04:00:00
 #SBATCH -A m3253
-#SBATCH --nodes 64
+#SBATCH --nodes 100
 
 setup_heptrkx
 
 which python
-srun -n 64 make_doublets_from_NNs configs/data_5000evts.yaml
+srun -n 200 make_doublets_from_NNs configs/data_5000evts.yaml
