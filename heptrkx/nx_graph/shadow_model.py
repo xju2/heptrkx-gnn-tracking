b67425169a516b7dff4fb3b69f8ba49974ce4725
xju <xju@cern.ch>
2019-09-25 15:19:58 -0700

    different NN model for doublets

--- heptrkx/nx_graph/shadow_model.py
+++ heptrkx/nx_graph/shadow_model.py
@@ -8,3 +8,15 @@ def fully_connected_classifier():
             keras.layers.Dense(32, activation=tf.nn.relu),
             keras.layers.Dense(1, activation=tf.nn.sigmoid)])
     return model
+
+
+def fc_with_dropout():
+    model = keras.Sequential([
+            keras.layers.Dense(128, activation='relu'),
+            keras.layers.Dropout(0.2),
+            keras.layers.Dense(64, activation='relu'),
+            keras.layers.Dropout(0.1),
+            keras.layers.Dense(32, activation='relu'),
+            keras.layers.Dense(1, activation='sigmoid')])
+    return model
+
