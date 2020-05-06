93d0ada3137dc48dc9da43cb8569cf66190780b7
xju <xju@cern.ch>
2020-05-05 17:14:16 -0700

    scripts to use embedded segments

--- heptrkx/nx_graph/__init__.py
+++ heptrkx/nx_graph/__init__.py
@@ -6,5 +6,9 @@ from __future__ import division
 from __future__ import print_function
 
 from heptrkx.nx_graph.model import SegmentClassifier as mm
+from heptrkx.nx_graph.model_vary2 import SegmentClassifier as mm2
+
 def get_model(model_name=None):
+    if model_name == 'vary2':
+        return mm2()
     return mm()
