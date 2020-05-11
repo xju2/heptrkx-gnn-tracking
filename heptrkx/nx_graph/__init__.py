54f0cd6efe02fbc9e01210be707faa42835a7b7c
Xiangyang Ju <xiangyang.ju@gmail.com>
2020-05-11 03:22:54 +0000

    add vary3

--- heptrkx/nx_graph/__init__.py
+++ heptrkx/nx_graph/__init__.py
@@ -7,8 +7,11 @@ from __future__ import print_function
 
 from heptrkx.nx_graph.model import SegmentClassifier as mm
 from heptrkx.nx_graph.model_vary2 import SegmentClassifier as mm2
+from heptrkx.nx_graph.model_vary3 import SegmentClassifier as mm3
 
 def get_model(model_name=None):
     if model_name == 'vary2':
         return mm2()
+    if model_name == "vary3":
+        return mm3()
     return mm()
